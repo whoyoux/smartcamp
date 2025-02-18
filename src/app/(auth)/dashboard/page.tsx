385dc3806d3725f12cb8cfcd9b211f001643a512
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-02-18 20:37:05 +0100

    moved campaign card to another file

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import CampaignCard from "@/components/campaign-card";
 import CreateNewCampaign from "@/components/create-new-campaign";
 import getSession from "@/lib/auth-server";
 import { prisma } from "@/lib/prisma";
@@ -28,19 +29,7 @@ export default async function DashboardPage() {
 			<div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 				{campaigns.map((cam) => (
 					<Link key={cam.id} href={`/dashboard/campaign/${cam.id}`}>
-						<div className="w-full aspect-video cursor-pointer bg-card rounded-lg p-4 flex flex-col justify-between border">
-							<div>
-								<h3 className="text-lg font-semibold truncate">{cam.name}</h3>
-								<p className="text-muted-foreground truncate">
-									{cam.description}
-								</p>
-							</div>
-							<div>
-								<span className="text-sm text-muted-foreground truncate">
-									{new Date(cam.createdAt).toLocaleString()}
-								</span>
-							</div>
-						</div>
+						<CampaignCard campaign={cam} />
 					</Link>
 				))}
 			</div>
---
+++ src/components/campaign-card.tsx
@@ -0,0 +1,17 @@
+import type { Campaign } from "@prisma/client";
+
+export default function CampaignPage({ campaign }: { campaign: Campaign }) {
+	return (
+		<div className="w-full aspect-video cursor-pointer bg-card rounded-lg p-4 flex flex-col justify-between border">
+			<div>
+				<h3 className="text-lg font-semibold truncate">{campaign.name}</h3>
+				<p className="text-muted-foreground truncate">{campaign.description}</p>
+			</div>
+			<div>
+				<span className="text-sm text-muted-foreground truncate">
+					{new Date(campaign.createdAt).toLocaleString()}
+				</span>
+			</div>
+		</div>
+	);
+}
